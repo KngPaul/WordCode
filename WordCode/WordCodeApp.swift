67fdd1e24fdacb58b2b8949cb4d862e9a6f1e92c
Paul Onawola <onawola.ire@gmail.com>
2023-01-02 23:11:06 +0100

    Initial Commit

---
+++ WordCode/WordCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordCodeApp.swift
+//  WordCode
+//
+//  Created by Paul Onawola on 02/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
